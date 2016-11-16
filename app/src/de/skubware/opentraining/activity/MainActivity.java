cb9dd05b899b7e3607fdd7e12fee86acfec0f4d6
Igor Bobko <limit-speed@yandex.ru>
2016-11-16 20:01:26 +0300

    Database Implementation

--- app/src/de/skubware/opentraining/activity/MainActivity.java
+++ app/src/de/skubware/opentraining/activity/MainActivity.java
@@ -18,21 +18,11 @@
 
 package de.skubware.opentraining.activity;
 
-import java.io.IOException;
-import java.util.List;
-
-import de.skubware.opentraining.R;
-import de.skubware.opentraining.activity.create_workout.ExerciseTypeListActivity;
-import de.skubware.opentraining.activity.manage_workouts.WorkoutListActivity;
-import de.skubware.opentraining.activity.settings.SettingsActivity;
-import de.skubware.opentraining.basic.Workout;
-import de.skubware.opentraining.db.Cache;
-import de.skubware.opentraining.db.DataProvider;
-import de.skubware.opentraining.db.IDataProvider;
-
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.preference.PreferenceManager;
@@ -46,44 +36,42 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Toast;
 
-import com.examples.youtubeapidemo.ActionBarDemoActivity;
-import com.examples.youtubeapidemo.DeveloperKey;
-import com.examples.youtubeapidemo.FragmentDemoActivity;
-import com.examples.youtubeapidemo.FullscreenDemoActivity;
-import com.examples.youtubeapidemo.IntentsDemoActivity;
-import com.examples.youtubeapidemo.PlayerControlsDemoActivity;
-import com.examples.youtubeapidemo.PlayerViewDemoActivity;
-import com.examples.youtubeapidemo.StandalonePlayerDemoActivity;
 import com.examples.youtubeapidemo.VideoListDemoActivity;
-import com.examples.youtubeapidemo.VideoWallDemoActivity;
-import com.examples.youtubeapidemo.YouTubeAPIDemoActivity;
-import com.examples.youtubeapidemo.YouTubeFailureRecoveryActivity;
-import com.google.api.client.http.HttpRequest;
-import com.google.api.client.http.HttpRequestInitializer;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.services.youtube.YouTube;
-import com.google.api.services.youtube.model.SearchListResponse;
-import com.google.api.services.youtube.model.SearchResult;
+import com.idescout.sql.SqlScoutServer;
+
+import java.util.List;
 
 import at.technikum.mti.fancycoverflow.FancyCoverFlow;
+import de.skubware.opentraining.R;
+import de.skubware.opentraining.activity.create_workout.ExerciseTypeListActivity;
+import de.skubware.opentraining.activity.manage_workouts.WorkoutListActivity;
+import de.skubware.opentraining.activity.settings.SettingsActivity;
+import de.skubware.opentraining.basic.Workout;
+import de.skubware.opentraining.database.DatabaseForCamel;
+import de.skubware.opentraining.database.WeightEntity;
+import de.skubware.opentraining.db.Cache;
+import de.skubware.opentraining.db.DataProvider;
+import de.skubware.opentraining.db.IDataProvider;
 
 
 public class MainActivity extends ActionBarActivity {
-    /** Tag for logging */
+    /**
+     * Tag for logging
+     */
     public static final String TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        SqlScoutServer.create(this, getPackageName());
+
         int SDK_INT = android.os.Build.VERSION.SDK_INT;
-        if (SDK_INT > 8)
-        {
+        if (SDK_INT > 8) {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
                     .permitAll().build();
             StrictMode.setThreadPolicy(policy);
-            //your codes here
+
 
         }
 
@@ -112,6 +100,25 @@ public class MainActivity extends ActionBarActivity {
         if (showDisclaimer) {
             new DisclaimerDialog(this);
         }
+
+        final DatabaseForCamel databaseForCamel = new DatabaseForCamel(getApplicationContext());
+        SQLiteDatabase db = databaseForCamel.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("d", "1");
+        values.put("w", 100);
+
+
+
+        long newRowId = db.insert(WeightEntity.TABLE_NAME,null,values);
+
+
+
+
+
+        //databaseForCamel.
+        //databaseForCamel.getWritableDatabase().execSQL("INSERT INTO (id,d,w) values('1','1','1')");
+
 
     }
 
@@ -145,48 +152,48 @@ public class MainActivity extends ActionBarActivity {
                     case 4: // Video1
                         startActivity(new Intent(MainActivity.this.getApplicationContext(), VideoListDemoActivity.class));
                         break;
-                    case 5: // Video2
-                        startActivity(new Intent(MainActivity.this.getApplicationContext(), ActionBarDemoActivity.class));
-                        break;
-                    case 6: // Video3
-                        startActivity(new Intent(MainActivity.this.getApplicationContext(), FragmentDemoActivity.class));
-                        break;
-                    case 7: // Video4
-                        startActivity(new Intent(MainActivity.this.getApplicationContext(), FullscreenDemoActivity.class));
-
-
-
-                    case 8: // Video5
-                        startActivity(new Intent(MainActivity.this.getApplicationContext(), IntentsDemoActivity.class));
-                        break;
-
-                    case 9: // Video6
-                        startActivity(new Intent(MainActivity.this.getApplicationContext(), PlayerViewDemoActivity.class));
-                        break;
-
-                    case 10: // Video7
-                        startActivity(new Intent(MainActivity.this.getApplicationContext(), StandalonePlayerDemoActivity.class));
-                        break;
-
-                    case 11: // Video8
-                        startActivity(new Intent(MainActivity.this.getApplicationContext(), VideoListDemoActivity.class));
-                        break;
-
-                    case 12: // Video9
-                        startActivity(new Intent(MainActivity.this.getApplicationContext(), VideoWallDemoActivity.class));
-                        break;
-
-                    case 13: // Video10
-                        startActivity(new Intent(MainActivity.this.getApplicationContext(), YouTubeAPIDemoActivity.class));
-                        break;
-
-                    case 14: // Video11
-                        startActivity(new Intent(MainActivity.this.getApplicationContext(), YouTubeFailureRecoveryActivity.class));
-                        break;
-
-                    case 15: // Video12
-                        startActivity(new Intent(MainActivity.this.getApplicationContext(), SearchingActivity.class));
-                        break;
+//                    case 5: // Video2
+//                        startActivity(new Intent(MainActivity.this.getApplicationContext(), ActionBarDemoActivity.class));
+//                        break;
+//                    case 6: // Video3
+//                        startActivity(new Intent(MainActivity.this.getApplicationContext(), FragmentDemoActivity.class));
+//                        break;
+//                    case 7: // Video4
+//                        startActivity(new Intent(MainActivity.this.getApplicationContext(), FullscreenDemoActivity.class));
+//
+//
+//
+//                    case 8: // Video5
+//                        startActivity(new Intent(MainActivity.this.getApplicationContext(), IntentsDemoActivity.class));
+//                        break;
+//
+//                    case 9: // Video6
+//                        startActivity(new Intent(MainActivity.this.getApplicationContext(), PlayerViewDemoActivity.class));
+//                        break;
+//
+//                    case 10: // Video7
+//                        startActivity(new Intent(MainActivity.this.getApplicationContext(), StandalonePlayerDemoActivity.class));
+//                        break;
+//
+//                    case 11: // Video8
+//                        startActivity(new Intent(MainActivity.this.getApplicationContext(), VideoListDemoActivity.class));
+//                        break;
+//
+//                    case 12: // Video9
+//                        startActivity(new Intent(MainActivity.this.getApplicationContext(), VideoWallDemoActivity.class));
+//                        break;
+//
+//                    case 13: // Video10
+//                        startActivity(new Intent(MainActivity.this.getApplicationContext(), YouTubeAPIDemoActivity.class));
+//                        break;
+//
+//                    case 14: // Video11
+//                        startActivity(new Intent(MainActivity.this.getApplicationContext(), YouTubeFailureRecoveryActivity.class));
+//                        break;
+//
+//                    case 15: // Video12
+//                        startActivity(new Intent(MainActivity.this.getApplicationContext(), SearchingActivity.class));
+//                        break;
 
 
                     default:
@@ -199,7 +206,9 @@ public class MainActivity extends ActionBarActivity {
     }
 
 
-    /** Shows a dialog for choosing a {@link Workout} */
+    /**
+     * Shows a dialog for choosing a {@link Workout}
+     */
     private void showSelectWorkoutDialog() {
         IDataProvider dataProvider = new DataProvider(this);
 
